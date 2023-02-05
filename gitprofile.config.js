49867dc5545e5055e7cb8e07f75a94822be22e1e
bomber327 <121346166+bomber327@users.noreply.github.com>
2023-02-06 02:13:51 +0300

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -109,11 +109,11 @@ const config = {
   },
   themeConfig: {
   //  defaultTheme: 'winter',
- defaultTheme: 'Bumblebee',
+ defaultTheme: 'bumblebee',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
-    disableSwitch: false,
+    disableSwitch: true,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
